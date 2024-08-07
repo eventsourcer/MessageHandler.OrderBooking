a68caf8d7d49a6e5650f59afcad5806f1b44c6a4
Sarwan Surchi <sarwan.h.najim@gmail.com>
2024-08-07 21:42:39 +0300

    Clean the mess up

--- OrderBooking.Api/Program.cs
+++ OrderBooking.Api/Program.cs
@@ -19,42 +19,14 @@ var builder = WebApplication.CreateBuilder(args);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
-// var eventSourcingConfig = new EventsourcingRuntime();
-// var runtimeConfig = new HandlerRuntimeConfiguration();
-// var handlerRuntime = new MessageHandler.Runtime.HandlerRuntime();
-// // var root = new MessageHandler.Runtime.ConfigurationRoot();
-
-// class ConfRoot : EventsourcingRuntime
-// {
-//     void Root()
-//     {
-//         var rt = HandlerName;
-//     }
-// }
-// class SettingsRoott : ProjectionsRestorer
-// {
-//     void Root()
-//     {
-//         settings
-//     }
-// }
-//  class MyRUnTime : TokenContent
-// {
-//     void D()
-//     {
-//         var tr = base.GetLicensingToken;
-//     }
-// }
-
-
-
+ 
 builder.Services.AddMessageHandler(nameof(OrderBookingAggregate), runtimeConfiguration =>
 {
-    runtimeConfiguration.License(builder.Configuration["LicenseToken"]);
-    var connectionString = builder.Configuration.Get<string>("TableStorageConnection")
+    var connectionString = builder.Configuration.GetValue<string>("TableStorageConnection")
                                    ?? throw new Exception("No 'TableStorageConnection' was provided. Use User Secrets or specify via environment variable.");
 
+    runtimeConfiguration.License(builder.Configuration["LicenseToken"]);
+    
     runtimeConfiguration.EventSourcing(source =>
     {
         source.Stream(nameof(OrderBookingAggregate),
@@ -68,17 +40,6 @@ builder.Services.AddMessageHandler(nameof(OrderBookingAggregate), runtimeConfigu
     });
 });
 
-
-
-// builder.Services.AddCors(options =>
-// {
-//     options.AddPolicy("AllowAll", policy =>
-//     {
-//         policy.AllowAnyMethod()
-//               .AllowAnyOrigin()
-//               .AllowAnyHeader();
-//     });
-// });
 
 var app = builder.Build();
 
@@ -115,58 +76,4 @@ app.MapGet("api/orderbookingdetailed", async(IRestoreProjections<BookingDetail> 
     Results.Ok(await projector.Restore(nameof(OrderBookingAggregate), id))
 );
 
-app.MapGet("test", async(IServiceProvider serviceProvider, IConfiguration conf) =>
-{
-    var confs = serviceProvider.GetServices(conf.GetType());
-    var manager = new ConfigurationManager();
-    // var c = new Conf();
-    await Conf.Go(serviceProvider);
-});
-
 app.Run();
-
-static class Conf
-{
-    internal static string GetLicensingToken(this MessageHandler.Runtime.ConfigurationRoot configuration)
-    {
-        return "messagehandler.licensetoken";
-    }
-    public static string GetValue<T>(this IConfiguration configuration, string key)
-    {
-        return configuration.GetSection(key).Get<string>();
-    }
-    public static string Bind(this IConfiguration configuration, object? key)
-    {
-        return configuration.GetSection(key.ToString()).Get<string>();
-    }
-    public static string Bind(this IConfiguration configuration, object? key, Action<BinderOptions>? options)
-    {
-        return configuration.GetSection(key.ToString()).Get<string>();
-    }
-    public static string Bind(this IConfiguration configuration, string key, object? binder)
-    {
-        return configuration.GetSection(key.ToString()).Get<string>();
-    }
-    public static string GetValue(this IConfiguration configuration, Type type, string key)
-    {
-        return configuration.GetSection(key).Get<string>();
-    }
-    public static string Get<T>(this IConfiguration configuration, string key)
-    {
-        return configuration.GetSection(key).Get<string>();
-    }
-    public static string Get(this IConfiguration configuration, Type type, string key)
-    {
-        return configuration.GetSection(key).Get<string>();
-    }
-    public static string GetSection(this IConfiguration configuration, string key)
-    {
-        return "configuration.GetSection(key).Get<string>();";
-    }
-    public static async Task Go(IServiceProvider serviceProvider)
-    {
-        var service = serviceProvider.GetServices<IHostedService>().First(x => x.GetType().Name == "CheckLicense");
-        
-        await service.StartAsync(new CancellationToken());
-    }
-}
